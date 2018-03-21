343738c79d91dcd4b7709079d6ce5b997f483fb4
Jie-Wei <swiftywu@yahoo.com>
2018-03-21 18:28:51 -0400

    Send data through websocket: rotation, position, one eventlistener

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const WebSocket = require('ws')
 
 const PORT = 3000;
 
-const server = http.createServer(app)
-const wss = new WebSocket.Server({server})
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+let position = null;
+let rotation = null;
 
 wss.on('connection', function (ws) {
-    ws.on('message', (message) => {
-        console.log(typeof(message))
-        message = JSON.parse(message)
-        console.log(message['x'])
+    ws.on('message', (data) => {
+        data = JSON.parse(data)
+        position = data['position'];
+        rotation = data['rotation'];
+
+        if ('triggerdown' in data) {
+
+        }
     })
     ws.on('close', () => clearInterval(intervalId))
 
