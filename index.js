e278f029d6e507b8cd52bece9e7769b23fd15b1c
Jie-Wei <swiftywu@yahoo.com>
2018-03-23 09:20:12 -0400

    Server handle helper/worker response structure

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ const app = express();
 const http = require('http')
 const WebSocket = require('ws')
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 let position = null;
 let rotation = null;
+let helperWS;
+let workerWS;
 
 wss.on('connection', function (ws) {
     ws.on('message', (data) => {
-        data = JSON.parse(data)
-        position = data['position'];
-        rotation = data['rotation'];
-
-        if ('triggerdown' in data) {
-
+        console.log('data: ', data);
+        if (data === 'HELPER') {
+            helperWS = ws;
+        } else if (data === 'WORKER') {
+            workerWS = ws;
+        } else {
+            data = JSON.parse(data);
+            position = data['position'];
+            rotation = data['rotation'];
         }
     })
     ws.on('close', () => clearInterval(intervalId))
-
+    ws.on('error', (err) => console.log(err));
     let intervalId = setInterval(
         () => ws.send(`${new Date()}`),
         1000
     )
-})
+});
 
-app.use(express.static('client'));
-
-server.listen(PORT);
-console.log(`Listening on port ${PORT}`);
+app.use('/', express.static('client'));
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
