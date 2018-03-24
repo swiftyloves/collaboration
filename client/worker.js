5a363fe2fd3281c5dc57485477a4e6703845e389
Jie-Wei <swiftywu@yahoo.com>
2018-03-24 13:01:04 -0400

    Separate helper and worker socket

--- client/worker.js
+++ client/worker.js
@@ -2,6 +2,9 @@ let ws = new WebSocket('ws://' + window.location.host);
 
 ws.onopen = function () {
     ws.send('WORKER_WS');
+    setInterval(() => {
+        // ws.send(JSON.stringify())
+    }, 100)
 }
 
 ws.onmessage = function (ev) {
--- index.js
+++ index.js
@@ -17,12 +17,18 @@ wss.on('connection', function (ws) {
         console.log('data: ', data);
         if (data === 'HELPER_WS') {
             helperWS = ws;
+            helperWS.on('message', (data) => {
+                // Helepr hand data
+                data = JSON.parse(data);
+                position = data['position'];
+                rotation = data['rotation'];
+            })
         } else if (data === 'WORKER_WS') {
             workerWS = ws;
-        } else {
-            data = JSON.parse(data);
-            position = data['position'];
-            rotation = data['rotation'];
+            workerWS.on('message', (data) => {
+                // Worker video data
+                console.log('worker data: ', data);
+            })
         }
     })
     ws.on('close', () => clearInterval(intervalId))
